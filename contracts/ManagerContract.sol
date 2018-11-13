d5f8e1158ced1506edf1c2a38f6e3795c4e704dc
Andrzej Wódkiewicz <akwodkiewicz@gmail.com>
2018-11-13 01:28:27 +0100

    MC: add 2 events
    Transactions cannot return values, so 2 events were added, to provide
    basic information about created categories and votings.

--- contracts/ManagerContract.sol
+++ contracts/ManagerContract.sol
@@ -10,6 +10,8 @@ contract ManagerContract {
     mapping(address => bool) public doesCategoryExist;
     mapping(bytes32 => address) public categoryAddress;
 
+    event CategoryCreated(address creator, address categoryAddress, bytes32 categoryName);
+    event VotingCreated(address creator, address votingAddress, string question);
 
     function createVotingWithExistingCategory(address _category,
         string _question,
@@ -17,12 +19,12 @@ contract ManagerContract {
         uint256 _votingEndTime,
         uint256 _resultsEndTime,
         bool _isPrivate,
-        address[] _permissions) public returns(address) {
+        address[] _permissions) public {
 
         require(doesCategoryExist[_category], "Provided addresss does not point to an existing category!");
         CategoryContract cc = CategoryContract(_category);
         address vcAddress = cc.createVotingContract(_question, _options, _votingEndTime, _resultsEndTime, _isPrivate, _permissions);
-        return vcAddress;
+        emit VotingCreated(msg.sender, vcAddress, _question);
     }
 
     function createVotingWithNewCategory(bytes32 _categoryName,
@@ -31,7 +33,7 @@ contract ManagerContract {
         uint256 _votingEndTime,
         uint256 _resultsEndTime,
         bool _isPrivate,
-        address[] _permissions) public returns(address) {
+        address[] _permissions) public {
         
         require(categoryAddress[_categoryName] == 0, "This category name is already used");
 
@@ -39,8 +41,9 @@ contract ManagerContract {
         categoryAddress[_categoryName] = address(cc);
         doesCategoryExist[address(cc)] = true;
         categoryContractsList.push(address(cc));
+        emit CategoryCreated(msg.sender, address(cc), _categoryName);
 
         address vcAddress = cc.createVotingContract(_question, _options, _votingEndTime, _resultsEndTime, _isPrivate, _permissions);
-        return vcAddress;
+        emit VotingCreated(msg.sender, vcAddress, _question);
     }
 }
