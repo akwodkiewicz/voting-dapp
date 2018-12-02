cc14ffa8bf23b5513f52c5527577eedb248f40e2
zyngielg <gustaw.zyngiel@gmail.com>
2018-12-02 17:55:45 +0100

    wip on CreateVotePage: added dynamic list of answers

---
+++ client/src/components/createVote/AnswersList.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import {Link} from 'react-router-dom';
+
+class AnswersList extends Component {
+  render() {
+    return (
+      <div className="container">
+            <ul className="list-group text-center">
+              {
+                Object.keys(this.props.answers).map(function(key) {
+                  return <li className="list-group-item">{this.props.answers[key]}</li>
+                }.bind(this))
+              }
+            </ul>
+           </div>
+    );
+  }
+}
+
+
+export default AnswersList;
---
+++ client/src/components/createVote/CreateVotePage.js
@@ -0,0 +1,125 @@
+import React, { Component } from "react";
+import {Link} from 'react-router-dom';
+import AnswersList from "./AnswersList";
+
+class CreateVotePage extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      question: '',
+      category: 'aaa',
+      answers: [],
+      voteType: 'public'      
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.changeVoteType = this.changeVoteType.bind(this);
+    this.addAnswer = this.addAnswer.bind(this);
+  }
+
+
+
+  
+  handleCreateVote() {    
+  }
+  
+  addAnswer({target}) {
+      var answer = document.getElementById('answer').value;
+      var allAnswers = this.state.answers;
+      allAnswers.push(answer);
+      this.setState(() => ({
+        answers : allAnswers
+      })
+    )
+  }
+
+  handleChange({target}) {
+    this.setState(() => ({
+      [target.name]: target.value
+    }))
+    console.log(this.state.question);
+    console.log(this.state.category);
+  }
+
+  changeVoteType({target}) {
+    var isPublic = document.getElementById('option-public').checked;
+    var voteType = isPublic ? 'public' : 'private';
+    console.log(voteType);
+    this.setState(() => ({
+      [voteType]: voteType
+    }))
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleCreateVote}>
+       
+        <div className="form-group">
+          <label htmlFor="question">Question</label>
+          <input type="text" name = "question" className="form-control" id="question" placeholder="Type a question for vote" onChange={this.handleChange} value={this.state.question}/>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="categories">Categories</label>
+            <input id="categories" 
+              type="text" 
+              className="form-control" 
+              name="city" 
+              onChange={this.handleChange}
+              placeholder="Select existing category or type a new one" 
+              list="existing-categories"/>
+            <datalist  id="existing-categories">
+              <option value="Arts"/>
+              <option value="Politics"/>
+              <option value="Business"/>
+            </datalist>          
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="answers">Answers</label>
+          <small id="answersHelp" className="form-text text-muted">There must be at least two answers.</small>
+
+          <AnswersList answers={this.state.answers}/>
+
+          <div className="input-group">
+            <input type="text" id="answer" className="form-control" placeholder="Type an answer"/>
+            <div className="input-group-append">
+              <button type="button" className="btn btn-outline-secondary" onClick={this.addAnswer}>Add</button>                       
+            </div>
+          </div>
+        </div>
+
+        <fieldset className="form-group">
+            <legend>Vote type</legend>
+            <div className="form-check">
+              <label className="form-check-label">
+                <input type="radio" className="form-check-input" name="optionsRadios" id="option-public" onChange={this.changeVoteType} checked/>
+                Public
+              </label>
+            </div>
+            <div className="form-check">
+            <label className="form-check-label">
+                <input type="radio" className="form-check-input" name="optionsRadios" id="option-private" onChange={this.changeVoteType}/>
+                Private
+              </label>
+            </div>
+        </fieldset>
+
+        <PrivateAdressTextBox voteType={this.state.voteType} />
+
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
+    );
+  }
+
+}
+
+function PrivateAdressTextBox(props) {
+  const voteType = props.voteType;
+  if(voteType === 'private') {
+    return <h1> LOL </h1>
+  }  
+  return null;  
+}
+
+export default CreateVotePage;
