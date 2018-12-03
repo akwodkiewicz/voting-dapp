d00057c5804fb0ccf0cdf509f0e78c53ab956c09
Andrzej Wódkiewicz <akwodkiewicz@gmail.com>
2018-12-03 21:30:16 +0100

    lint test files

--- test/consts.ts
+++ test/consts.ts
@@ -1,3 +1,3 @@
-export const ManagerContract = artifacts.require('ManagerContract');
-export const CategoryContract = artifacts.require('CategoryContract');
-export const VotingContract = artifacts.require('VotingContract');
+export const ManagerContract = artifacts.require("ManagerContract");
+export const CategoryContract = artifacts.require("CategoryContract");
+export const VotingContract = artifacts.require("VotingContract");
