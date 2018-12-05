c7b848dd9c0164e43f40337397671afc5f0cd6dd
zyngielg <gustaw.zyngiel@gmail.com>
2018-12-05 19:04:03 +0100

    add day validation while picking voting end time and results viewing end time; TODO: hour and minute validation

--- client/src/components/createVote/VoteDates.js
+++ client/src/components/createVote/VoteDates.js
@@ -1,11 +1,9 @@
 import React, { Component } from "react";
-import {ControlLabel, FormGroup, Radio, FormControl, Grid, Row, Col }from 'react-bootstrap';
+import {ControlLabel, FormGroup, Grid, Row, Col }from 'react-bootstrap';
 import * as Datetime from 'react-datetime';
+import moment from "moment";
 
 class VoteDates extends Component {
-
-  
-
   render() {
     return (
       <FormGroup>
@@ -20,10 +18,31 @@ class VoteDates extends Component {
           </Row>
           <Row className="showGrid">
             <Col xs={6}> 
-              <Datetime/>
+              <Datetime 
+                inputProps={{id: 'votePeriodEnd'}}
+                closeOnSelect={true}
+                timeFormat={'HH:mm'}
+                isValidDate = {(current) => {   
+                  let resultsPeriodEnd = document.getElementById('resultsPeriodEnd');
+                  if(resultsPeriodEnd != null && resultsPeriodEnd.value !== "") {
+                    return moment(resultsPeriodEnd.value).isAfter(current.add(1, 'minute'), 'minute') && current.isAfter(moment().subtract(1, 'day'), 'minute');
+                  }
+                  return current.isAfter(moment().subtract(1, 'day'), 'minute');                 
+                }} 
+              />
             </Col>
             <Col xs={6}> 
-              <Datetime/>
+              <Datetime 
+                inputProps={{id: 'resultsPeriodEnd'}}
+                closeOnSelect={true}
+                isValidDate = {(current) => {   
+                  let votePeriodEnd = document.getElementById('votePeriodEnd');
+                  if(votePeriodEnd != null && votePeriodEnd.value !== "") {
+                    return current.isAfter(moment(votePeriodEnd.value).add(1, 'minute'), 'minute') && current.isAfter(moment().subtract(1, 'day'), 'minute');
+                  }
+                  return current.isAfter(moment().subtract(1, 'day'), 'minute');                 
+                }}
+              />
             </Col>
           </Row>
         </Grid>
