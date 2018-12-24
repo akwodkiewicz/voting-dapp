110f79ba03dd2f884864db956cec73794a695fca
Andrzej Wódkiewicz <akwodkiewicz@gmail.com>
2018-12-24 02:46:59 +0100

    fix tests

--- test/consts.ts
+++ test/consts.ts
@@ -1,3 +1,5 @@
-export const ManagerContract = artifacts.require("ManagerContract");
-export const CategoryContract = artifacts.require("CategoryContract");
-export const VotingContract = artifacts.require("VotingContract");
+import { CategoryContractContract, ManagerContractContract, VotingContractContract } from "../types/truffle-contracts";
+
+export const ManagerContract = artifacts.require("ManagerContract") as ManagerContractContract;
+export const CategoryContract = artifacts.require("CategoryContract") as CategoryContractContract;
+export const VotingContract = artifacts.require("VotingContract") as VotingContractContract;
