043e578dc2f708cc6ece51ec696c796f25dc0470
zyngielg <gustaw.zyngiel@gmail.com>
2018-12-30 00:36:41 +0100

    add modal rendering to HomePage based on the results of fetching data

--- client/src/components/home/HomePage.tsx
+++ client/src/components/home/HomePage.tsx
@@ -1,29 +1,123 @@
-import React, { Component } from "react";
-import { Button, FormControl, FormGroup, InputGroup } from "react-bootstrap";
+import moment from "moment";
+import React, { Component, Fragment } from "react";
+import { Button, Col, FormControl, InputGroup, Row } from "react-bootstrap";
 import { fetchVoting } from "../../utils/eth";
-import { BlockchainData } from "../../utils/types";
+import { BlockchainData, Voting } from "../../utils/types";
+import ResultsModal from "../vote/ResultsModal";
+//import VoteModal from "../vote/VoteModal";
 
 interface IHomePageProps {
   blockchainData: BlockchainData;
 }
-export default class HomePage extends Component<IHomePageProps> {
+
+interface IHomePageState {
+  isDataRefreshRequested: boolean;
+  searchActionCalled: boolean;
+  showNotFoundModal: boolean;
+  showResultsModal: boolean;
+  showVoteModal: boolean;
+  voting: Voting;
+}
+
+export default class HomePage extends Component<IHomePageProps, IHomePageState> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isDataRefreshRequested: false,
+      searchActionCalled: false,
+      showNotFoundModal: false,
+      showResultsModal: false,
+      showVoteModal: false,
+      voting: null,
+    };
+  }
+
   public searchVoting = async () => {
     // TODO: simple address validation
     const address = (document.getElementById("address") as HTMLInputElement).value;
-    const voting = await fetchVoting(this.props.blockchainData, address);
-    return voting;
+    const fetchedVoting = await fetchVoting(this.props.blockchainData, address);
+
+    this.setState({
+      searchActionCalled: true,
+      voting: fetchedVoting,
+    });
+
+    if (fetchedVoting != null) {
+      const now = moment().utc().unix(); // prettier-ignore
+      if (now <= fetchedVoting.info.votingEndTime) {
+        this.setState({
+          showNotFoundModal: false,
+          showResultsModal: false,
+          showVoteModal: true,
+        });
+      } else if (now <= fetchedVoting.info.resultsEndTime) {
+        this.setState({
+          showNotFoundModal: false,
+          showResultsModal: true,
+          showVoteModal: false,
+        });
+      } else {
+        this.setState({
+          showNotFoundModal: true,
+          showResultsModal: false,
+          showVoteModal: false,
+        });
+      }
+    }
   };
 
   public render() {
+    let modal;
+    if (this.state.voting != null) {
+      if (this.state.showVoteModal) {
+        modal = (
+          // <VoteModal
+          //   voting={this.state.voting}
+          //   blockchainData={this.props.blockchainData}
+          //   chosenAnswer={this.state.chosenAnswer}
+          //   setChosenAnswerInParent={this.handleAnswerClick}
+          //   requestDataRefresh={() => this.setState({ isDataRefreshRequested: true })}
+          //   show={this.state.showVoteModal}
+          //   handleOnHide={() => this.setState({ showVoteModal: false })}
+          // />
+          <ResultsModal
+            voting={this.state.voting}
+            blockchainData={this.props.blockchainData}
+            requestDataRefresh={() => this.setState({ isDataRefreshRequested: true })}
+            show={this.state.showResultsModal}
+            handleOnHide={() => this.setState({ showVoteModal: false })}
+          />
+        );
+      } else {
+        modal = (
+          <ResultsModal
+            voting={this.state.voting}
+            blockchainData={this.props.blockchainData}
+            requestDataRefresh={() => this.setState({ isDataRefreshRequested: true })}
+            show={this.state.showResultsModal}
+            handleOnHide={() => this.setState({ showVoteModal: false })}
+          />
+        );
+      }
+    } else if (this.state.searchActionCalled) {
+      modal = <h1>NI MA TAKIEGO GŁOSOWANIA</h1>;
+    }
     return (
-      <FormGroup>
-        <InputGroup>
-          <FormControl type="text" id="address" />
-          <InputGroup.Button>
-            <Button onClick={this.searchVoting}>Before</Button>
-          </InputGroup.Button>
-        </InputGroup>
-      </FormGroup>
+      <Fragment>
+        <Row>
+          <Col md={12}>
+            <InputGroup>
+              <FormControl type="text" id="address" />
+              <InputGroup.Button>
+                <Button onClick={this.searchVoting}>Before</Button>
+              </InputGroup.Button>
+            </InputGroup>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12}>{modal}</Col>
+        </Row>
+      </Fragment>
     );
   }
 }
