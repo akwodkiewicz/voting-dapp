66b19a2201d75006be77792c2bc24b9fcc108852
zyngielg <gustaw.zyngiel@gmail.com>
2019-01-02 18:32:03 +0100

    add more colours (currently 10 available)

--- client/src/components/vote/ResultsPieChart.tsx
+++ client/src/components/vote/ResultsPieChart.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Cell, Pie, PieChart, Legend, ResponsiveContainer, Sector, Tooltip } from "recharts";
 import { Voting } from "../../utils/types";
+import ResultsTooltip from "./ResultsTooltip";
 
 interface IResultsPieChartProps {
   results: string[];
@@ -50,7 +51,18 @@ export default class ResultsPieChart extends Component<IResultsPieChartProps, IR
   };
 
   public render() {
-    const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
+    const COLORS = [
+      "#4FFF43",
+      "#FF7543",
+      "#438FFF",
+      "#DAFF43",
+      "#565A49",
+      "#DC35E8",
+      "#99FFCC",
+      "#B2A23A",
+      "#000066",
+      "#006666",
+    ];
     return (
       <ResponsiveContainer width="100%" height={400}>
         <PieChart>
