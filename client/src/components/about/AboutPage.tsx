8155849f8be92de4347848c2de2627763bc54a40
zyngielg <gustaw.zyngiel@gmail.com>
2019-01-08 00:50:22 +0100

    remove panels for more minimalistic layout purpose

--- client/src/components/about/AboutPage.tsx
+++ client/src/components/about/AboutPage.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Col, Grid, Panel, Row } from "react-bootstrap";
+import { Col, Grid, Row } from "react-bootstrap";
 
 export default class AboutPage extends Component {
   public render() {
@@ -15,53 +15,38 @@ export default class AboutPage extends Component {
         </Row>
         <Row>
           <Col md={4}>
-            <Panel>
-              <Panel.Heading>
-                <Panel.Title style={{ textAlign: "center" }}>Decentralized</Panel.Title>
-              </Panel.Heading>
-              <Panel.Body>
-                <p style={{ textAlign: "justify" }}>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nibh augue, suscipit a, scelerisque
-                  sed, lacinia in, mi. Cras vel lorem. Etiam pellentesque aliquet tellus. Phasellus pharetra nulla ac
-                  diam. Quisque semper justo at risus. Donec venenatis, turpis vel hendrerit interdum, dui ligula
-                  ultricies purus, sed posuere libero dui id orci. Nam congue, pede vitae dapibus aliquet, elit magna
-                  vulputate arcu, vel tempus metus leo non est.
-                </p>
-              </Panel.Body>
-            </Panel>
+            <h3 style={{ textAlign: "center" }}>Decentralized</h3>
+
+            <p style={{ textAlign: "justify" }}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nibh augue, suscipit a, scelerisque sed,
+              lacinia in, mi. Cras vel lorem. Etiam pellentesque aliquet tellus. Phasellus pharetra nulla ac diam.
+              Quisque semper justo at risus. Donec venenatis, turpis vel hendrerit interdum, dui ligula ultricies purus,
+              sed posuere libero dui id orci. Nam congue, pede vitae dapibus aliquet, elit magna vulputate arcu, vel
+              tempus metus leo non est.
+            </p>
           </Col>
           <Col md={4}>
-            <Panel>
-              <Panel.Heading>
-                <Panel.Title style={{ textAlign: "center" }}>Blockchain-based</Panel.Title>
-              </Panel.Heading>
-              <Panel.Body>
-                <p style={{ textAlign: "justify" }}>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nibh augue, suscipit a, scelerisque
-                  sed, lacinia in, mi. Cras vel lorem. Etiam pellentesque aliquet tellus. Phasellus pharetra nulla ac
-                  diam. Quisque semper justo at risus. Donec venenatis, turpis vel hendrerit interdum, dui ligula
-                  ultricies purus, sed posuere libero dui id orci. Nam congue, pede vitae dapibus aliquet, elit magna
-                  vulputate arcu, vel tempus metus leo non est.
-                </p>
-              </Panel.Body>
-            </Panel>
+            <h3 style={{ textAlign: "center" }}>Blockchain-based</h3>
+
+            <p style={{ textAlign: "justify" }}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nibh augue, suscipit a, scelerisque sed,
+              lacinia in, mi. Cras vel lorem. Etiam pellentesque aliquet tellus. Phasellus pharetra nulla ac diam.
+              Quisque semper justo at risus. Donec venenatis, turpis vel hendrerit interdum, dui ligula ultricies purus,
+              sed posuere libero dui id orci. Nam congue, pede vitae dapibus aliquet, elit magna vulputate arcu, vel
+              tempus metus leo non est.
+            </p>
           </Col>
 
           <Col md={4}>
-            <Panel>
-              <Panel.Heading>
-                <Panel.Title style={{ textAlign: "center" }}>Voting platform</Panel.Title>
-              </Panel.Heading>
-              <Panel.Body>
-                <p style={{ textAlign: "justify" }}>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nibh augue, suscipit a, scelerisque
-                  sed, lacinia in, mi. Cras vel lorem. Etiam pellentesque aliquet tellus. Phasellus pharetra nulla ac
-                  diam. Quisque semper justo at risus. Donec venenatis, turpis vel hendrerit interdum, dui ligula
-                  ultricies purus, sed posuere libero dui id orci. Nam congue, pede vitae dapibus aliquet, elit magna
-                  vulputate arcu, vel tempus metus leo non est.
-                </p>
-              </Panel.Body>
-            </Panel>{" "}
+            <h3 style={{ textAlign: "center" }}>Voting platform</h3>
+
+            <p style={{ textAlign: "justify" }}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nibh augue, suscipit a, scelerisque sed,
+              lacinia in, mi. Cras vel lorem. Etiam pellentesque aliquet tellus. Phasellus pharetra nulla ac diam.
+              Quisque semper justo at risus. Donec venenatis, turpis vel hendrerit interdum, dui ligula ultricies purus,
+              sed posuere libero dui id orci. Nam congue, pede vitae dapibus aliquet, elit magna vulputate arcu, vel
+              tempus metus leo non est.
+            </p>
           </Col>
         </Row>
         <Row>
@@ -69,17 +54,40 @@ export default class AboutPage extends Component {
             <h3
               style={{
                 textAlign: "center",
-                marginTop: "1em",
-                marginBottom: "1em",
+                marginTop: "2em",
+                marginBottom: "2em",
+              }}
+            >
+              How does it work?
+            </h3>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={8} mdOffset={2}>
+            <p style={{ textAlign: "justify" }}>
+              Decentralized Voting Platform was created by Andrzej Wódkiewicz and Gustaw Żyngiel, final year students
+              pursuing the BSc in Computer Science programme at Warsaw University of Technology, Faculty of Mathematics
+              and Information Science. The application is their thesis project, developed under the guidance of Senior
+              Lecturer Paweł Kotowski, their thesis supervisor.
+            </p>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12}>
+            <h3
+              style={{
+                textAlign: "center",
+                marginTop: "2em",
+                marginBottom: "2em",
               }}
             >
               Application's genesis
             </h3>
           </Col>
         </Row>
-        <Row>
-          <Col md={12}>
-            <p>
+        <Row style={{ marginBottom: "4em" }}>
+          <Col md={8} mdOffset={2}>
+            <p style={{ textAlign: "justify" }}>
               Decentralized Voting Platform was created by Andrzej Wódkiewicz and Gustaw Żyngiel, final year students
               pursuing the BSc in Computer Science programme at Warsaw University of Technology, Faculty of Mathematics
               and Information Science. The application is their thesis project, developed under the guidance of Senior
