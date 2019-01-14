7841b05d6c2c460ff74a15a8c54ba7a5e73dd220
Andrzej Wódkiewicz <akwodkiewicz@gmail.com>
2019-01-14 01:12:13 +0100

    Updates

---
+++ precache-manifest.232d7b6ecb6d3b9bc7948914c4c64aad.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b316d3d1c5968733df565cac69e3cea",
+    "url": "/voting-dapp/static/media/ethereum-logo.6b316d3d.png"
+  },
+  {
+    "revision": "7a6367a4ca834e8ee2fa",
+    "url": "/voting-dapp/static/js/main.7a6367a4.chunk.js"
+  },
+  {
+    "revision": "2e278d0d08076a1c35e4f124bdac8a97",
+    "url": "/voting-dapp/static/media/wrong-network.2e278d0d.png"
+  },
+  {
+    "revision": "cfc80ff4c276f53dc577",
+    "url": "/voting-dapp/static/js/runtime~main.cfc80ff4.js"
+  },
+  {
+    "revision": "6e763b2bb8023d637cf0475a63caa7e6",
+    "url": "/voting-dapp/static/media/distributed-voting-platform.6e763b2b.png"
+  },
+  {
+    "revision": "afcf309418288bae4f92dd51ea7d38a5",
+    "url": "/voting-dapp/static/media/no-metamask.afcf3094.png"
+  },
+  {
+    "revision": "79e4e98f1f244fe7211d7faccc6f49a3",
+    "url": "/voting-dapp/static/media/metamask-logo.79e4e98f.png"
+  },
+  {
+    "revision": "6043a999d51deb9103bce5695809fbef",
+    "url": "/voting-dapp/static/media/no-acceptance.6043a999.png"
+  },
+  {
+    "revision": "7efbb0cb0a4a28860d45",
+    "url": "/voting-dapp/static/js/1.7efbb0cb.chunk.js"
+  },
+  {
+    "revision": "7efbb0cb0a4a28860d45",
+    "url": "/voting-dapp/static/css/1.a3d3659e.chunk.css"
+  },
+  {
+    "revision": "9df56699f9edc5c3e8f931ba5e131aac",
+    "url": "/voting-dapp/index.html"
+  }
+];
