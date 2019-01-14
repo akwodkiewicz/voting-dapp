389432a98cd12adc00cf8e4f40344b0916ec48c3
Andrzej Wódkiewicz <akwodkiewicz@gmail.com>
2019-01-14 01:30:55 +0100

    Updates

---
+++ precache-manifest.81fe8d215fd8ef3c676ab49eef8b8814.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b316d3d1c5968733df565cac69e3cea",
+    "url": "/static/media/ethereum-logo.6b316d3d.png"
+  },
+  {
+    "revision": "596f06553e2ea653c6a7",
+    "url": "/static/js/main.596f0655.chunk.js"
+  },
+  {
+    "revision": "2e278d0d08076a1c35e4f124bdac8a97",
+    "url": "/static/media/wrong-network.2e278d0d.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8c3a0f52a61c1a465da14a4a6d9ae991",
+    "url": "/static/media/distributed-voting-platform.8c3a0f52.png"
+  },
+  {
+    "revision": "afcf309418288bae4f92dd51ea7d38a5",
+    "url": "/static/media/no-metamask.afcf3094.png"
+  },
+  {
+    "revision": "79e4e98f1f244fe7211d7faccc6f49a3",
+    "url": "/static/media/metamask-logo.79e4e98f.png"
+  },
+  {
+    "revision": "32caef4847562dbca185f0331fe8ae20",
+    "url": "/static/media/no-acceptance.32caef48.png"
+  },
+  {
+    "revision": "d61ffe38775fa05ccb45",
+    "url": "/static/js/1.d61ffe38.chunk.js"
+  },
+  {
+    "revision": "d61ffe38775fa05ccb45",
+    "url": "/static/css/1.a3d3659e.chunk.css"
+  },
+  {
+    "revision": "d638a12108dc4a2c3a7a80eb7b71ffe5",
+    "url": "/index.html"
+  }
+];
