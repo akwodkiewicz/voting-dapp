e00801532dde7c840a538d582c616801dbb5c451
Andrzej Wódkiewicz <akwodkiewicz@gmail.com>
2019-01-14 01:56:11 +0100

    Updates

---
+++ precache-manifest.31d121bee39d5ef3a20f5249a02fb69c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b316d3d1c5968733df565cac69e3cea",
+    "url": "/voting-dapp/static/media/ethereum-logo.6b316d3d.png"
+  },
+  {
+    "revision": "c6b75d911f156f4e8617",
+    "url": "/voting-dapp/static/js/main.c6b75d91.chunk.js"
+  },
+  {
+    "revision": "2e278d0d08076a1c35e4f124bdac8a97",
+    "url": "/voting-dapp/static/media/wrong-network.2e278d0d.png"
+  },
+  {
+    "revision": "cfc80ff4c276f53dc577",
+    "url": "/voting-dapp/static/js/runtime~main.cfc80ff4.js"
+  },
+  {
+    "revision": "8c3a0f52a61c1a465da14a4a6d9ae991",
+    "url": "/voting-dapp/static/media/distributed-voting-platform.8c3a0f52.png"
+  },
+  {
+    "revision": "afcf309418288bae4f92dd51ea7d38a5",
+    "url": "/voting-dapp/static/media/no-metamask.afcf3094.png"
+  },
+  {
+    "revision": "79e4e98f1f244fe7211d7faccc6f49a3",
+    "url": "/voting-dapp/static/media/metamask-logo.79e4e98f.png"
+  },
+  {
+    "revision": "32caef4847562dbca185f0331fe8ae20",
+    "url": "/voting-dapp/static/media/no-acceptance.32caef48.png"
+  },
+  {
+    "revision": "d61ffe38775fa05ccb45",
+    "url": "/voting-dapp/static/js/1.d61ffe38.chunk.js"
+  },
+  {
+    "revision": "d61ffe38775fa05ccb45",
+    "url": "/voting-dapp/static/css/1.a3d3659e.chunk.css"
+  },
+  {
+    "revision": "f1e0a17b8ffd310b1b9c668798e43487",
+    "url": "/voting-dapp/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.81fe8d215fd8ef3c676ab49eef8b8814.js"
+  "/voting-dapp/precache-manifest.31d121bee39d5ef3a20f5249a02fb69c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/voting-dapp/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
